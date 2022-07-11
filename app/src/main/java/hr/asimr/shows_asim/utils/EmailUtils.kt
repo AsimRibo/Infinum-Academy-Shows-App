c9db41205a77924e640423af04ede14295918c94
Asim Ribo <asim.ribo21@gmail.com>
2022-07-11 18:18:50 +0200

    Add display logic to WelcomeActivity

--- app/src/main/java/hr/asimr/shows_asim/utils/EmailUtils.kt
+++ app/src/main/java/hr/asimr/shows_asim/utils/EmailUtils.kt
@@ -2,4 +2,8 @@ package hr.asimr.shows_asim.utils
 
 private val EMAIL_REGEX by lazy { "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$".toRegex() }
 
+private val DOMAIN_START by lazy { "@" }
+
 fun isEmailValid(email: String) = EMAIL_REGEX.matches(email)
+
+fun loseEmailDomain(email: String) = email.subSequence(0, email.indexOf(DOMAIN_START)).toString()
