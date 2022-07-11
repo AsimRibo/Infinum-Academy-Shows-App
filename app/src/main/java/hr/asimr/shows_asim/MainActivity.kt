82b2ba64cda9b06ad32646226e1e8ae16d800deb
Asim Ribo <asim.ribo21@gmail.com>
2022-07-11 16:34:41 +0200

    Add binding to MainActivity

--- app/src/main/java/hr/asimr/shows_asim/MainActivity.kt
+++ app/src/main/java/hr/asimr/shows_asim/MainActivity.kt
@@ -2,10 +2,14 @@ package hr.asimr.shows_asim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.asimr.shows_asim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
