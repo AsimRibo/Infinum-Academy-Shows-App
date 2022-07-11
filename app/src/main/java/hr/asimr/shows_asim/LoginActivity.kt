fc945e46f37ba320fb661a704305c5dadeff03a8
Asim Ribo <asim.ribo21@gmail.com>
2022-07-11 17:25:13 +0200

    Add binding to LoginActivity

--- app/src/main/java/hr/asimr/shows_asim/LoginActivity.kt
+++ app/src/main/java/hr/asimr/shows_asim/LoginActivity.kt
@@ -2,9 +2,14 @@ package hr.asimr.shows_asim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.asimr.shows_asim.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
