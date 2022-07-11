839954ad96ee77b89cd1df67e003d8fbb9404825
Asim Ribo <asim.ribo21@gmail.com>
2022-07-11 17:22:22 +0200

    Add LoginActivity

---
+++ app/src/main/java/hr/asimr/shows_asim/LoginActivity.kt
@@ -0,0 +1,10 @@
+package hr.asimr.shows_asim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
