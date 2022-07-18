9100e745bc27f26b39af7694819e63415b8b9e6a
Asim Ribo <asim.ribo21@gmail.com>
2022-07-18 14:36:24 +0200

    Add ShowsActivity

---
+++ app/src/main/java/hr/asimr/shows_asim/ShowsActivity.kt
@@ -0,0 +1,11 @@
+package hr.asimr.shows_asim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shows)
+    }
+}
