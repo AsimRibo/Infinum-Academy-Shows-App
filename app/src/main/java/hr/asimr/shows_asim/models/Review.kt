0a5dd5c2727863d7392bfc66b17a33650077740e
Asim Ribo <asim.ribo21@gmail.com>
2022-07-18 14:42:56 +0200

    Make Show and Review classes Parcelable

--- app/src/main/java/hr/asimr/shows_asim/models/Review.kt
+++ app/src/main/java/hr/asimr/shows_asim/models/Review.kt
@@ -1,8 +1,12 @@
 package hr.asimr.shows_asim.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Review(
     val id: String,
     val rating: Int,
     val comment: String,
     val username: String
-)
+) : Parcelable
--- app/src/main/java/hr/asimr/shows_asim/models/Show.kt
+++ app/src/main/java/hr/asimr/shows_asim/models/Show.kt
@@ -2,12 +2,13 @@ package hr.asimr.shows_asim.models
 
 import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
 
-
+@Parcelize
 class Show(
     val id: String,
     val name: String,
     val description: String,
     @DrawableRes val imageResourceId: Int,
     val reviews: MutableList<Review>
-)
+) : Parcelable
