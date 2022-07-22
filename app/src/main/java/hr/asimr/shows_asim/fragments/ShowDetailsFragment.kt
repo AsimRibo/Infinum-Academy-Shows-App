a41e88c44bd5fb399c218b7c564828bc61824ab6
Asim Ribo <asim.ribo21@gmail.com>
2022-07-22 10:57:37 +0200

    Change groups visibility handling

--- app/src/main/java/hr/asimr/shows_asim/fragments/ShowDetailsFragment.kt
+++ app/src/main/java/hr/asimr/shows_asim/fragments/ShowDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -100,8 +101,8 @@ class ShowDetailsFragment : Fragment() {
     }
 
     private fun updateGroupsVisibility() {
-        binding.groupReviews.visibility = View.VISIBLE
-        binding.groupNoReview.visibility = View.GONE
+        binding.groupReviews.isVisible = true
+        binding.groupNoReview.isVisible = false
     }
 
     private fun updateShowRatings() {
