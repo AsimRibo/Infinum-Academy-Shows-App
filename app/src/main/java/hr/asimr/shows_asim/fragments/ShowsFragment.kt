5ef679173dcc45c687e2288bcbc11c370a78cee9
Asim Ribo <asim.ribo21@gmail.com>
2022-07-24 16:28:22 +0200

    Add ShowsViewModel

--- app/src/main/java/hr/asimr/shows_asim/fragments/ShowsFragment.kt
+++ app/src/main/java/hr/asimr/shows_asim/fragments/ShowsFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.core.content.edit
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -18,6 +19,7 @@ import hr.asimr.shows_asim.databinding.DialogAddReviewBinding
 import hr.asimr.shows_asim.databinding.DialogUserProfileBinding
 import hr.asimr.shows_asim.databinding.FragmentShowsBinding
 import hr.asimr.shows_asim.models.Show
+import hr.asimr.shows_asim.viewModels.ShowsViewModel
 import java.util.*
 
 class ShowsFragment : Fragment() {
@@ -27,32 +29,9 @@ class ShowsFragment : Fragment() {
     private lateinit var adapter: ShowsAdapter
     private lateinit var email: String
 
-    private val args by navArgs<ShowsFragmentArgs>()
+    private val viewModel by viewModels<ShowsViewModel>()
 
-    private val shows = listOf(
-        Show(
-            UUID.randomUUID().toString(),
-            "The Office",
-            "The Office is an American mockumentary sitcom television series that depicts the everyday work lives of office employees at the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company. It aired on NBC from March 24, 2005, to May 16, 2013, spanning a total of nine seasons.",
-            R.drawable.ic_office,
-            listOf()
-        ),
-        Show(
-            UUID.randomUUID().toString(),
-            "Stranger Things",
-            "Stranger Things is an American science fiction horror drama television series " +
-                "created by the Duffer Brothers that is streaming on Netflix. The brothers serve as showrunners and are executive producers along with Shawn Levy and Dan Cohen. The first season of the series was released on Netflix on July 15, 2016.",
-            R.drawable.ic_stranger_things,
-            listOf()
-        ),
-        Show(
-            UUID.randomUUID().toString(),
-            "Krv nije voda",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            R.drawable.ic_knv,
-            listOf()
-        )
-    )
+    private val args by navArgs<ShowsFragmentArgs>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
