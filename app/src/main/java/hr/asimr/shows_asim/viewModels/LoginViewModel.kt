f213e380584999efcb4d258e1011df9df460ffbc
Asim Ribo <asim.ribo21@gmail.com>
2022-07-28 00:41:11 +0200

    Add LoginVievModel and it's factory

---
+++ app/src/main/java/hr/asimr/shows_asim/viewModels/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package hr.asimr.shows_asim.viewModels
+
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel(preferences: SharedPreferences) : ViewModel() {
+}
