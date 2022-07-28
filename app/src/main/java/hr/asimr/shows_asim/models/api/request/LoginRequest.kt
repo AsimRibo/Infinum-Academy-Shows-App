60ddde5defa08dc3eb0ccaadceda38d230f1b43e
Asim Ribo <asim.ribo21@gmail.com>
2022-07-28 02:12:07 +0200

    Add serializable annotation

--- app/src/main/java/hr/asimr/shows_asim/models/api/request/LoginRequest.kt
+++ app/src/main/java/hr/asimr/shows_asim/models/api/request/LoginRequest.kt
@@ -1,7 +1,9 @@
 package hr.asimr.shows_asim.models.api.request
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class LoginRequest (
     @SerialName("email") val email: String,
     @SerialName("password") val password: String
