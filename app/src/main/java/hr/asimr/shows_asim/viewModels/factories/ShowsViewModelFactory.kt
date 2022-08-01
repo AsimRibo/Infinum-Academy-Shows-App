4bbb1346e41dfab1e0b5710b6fd49b62b8e70294
Asim Ribo <asim.ribo21@gmail.com>
2022-08-01 04:11:06 +0200

    Add factory for ShowsViewModel

---
+++ app/src/main/java/hr/asimr/shows_asim/viewModels/factories/ShowsViewModelFactory.kt
@@ -0,0 +1,16 @@
+package hr.asimr.shows_asim.viewModels.factories
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import hr.asimr.shows_asim.dao.ShowsDatabase
+import hr.asimr.shows_asim.viewModels.ShowsViewModel
+
+class ShowsViewModelFactory(val database: ShowsDatabase) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ShowsViewModel::class.java)) {
+            return ShowsViewModel(database) as T
+        }
+        throw IllegalArgumentException("Wrong type")
+    }
+}
