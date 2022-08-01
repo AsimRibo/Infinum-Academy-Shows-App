02d17707bc7d3ac717f1e20ad3b33f3c4da7bba4
Asim Ribo <asim.ribo21@gmail.com>
2022-08-01 03:50:22 +0200

    Add showsDatabase to application class

--- app/src/main/java/hr/asimr/shows_asim/ShowsApplication.kt
+++ app/src/main/java/hr/asimr/shows_asim/ShowsApplication.kt
@@ -2,10 +2,15 @@ package hr.asimr.shows_asim
 
 import android.app.Application
 import android.content.Context
+import hr.asimr.shows_asim.dao.ShowsDatabase
 import hr.asimr.shows_asim.fragments.LOGIN_PREFERENCES
 import hr.asimr.shows_asim.networking.ApiModule
 
 class ShowsApplication : Application() {
+
+    val showsDatabase by lazy {
+        ShowsDatabase.getDatabase(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
