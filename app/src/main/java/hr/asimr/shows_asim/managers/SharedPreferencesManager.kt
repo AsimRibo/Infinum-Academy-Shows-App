a356e2b0e1fac8b7e25aa6f94881afb4f14be803
Asim Ribo <asim.ribo21@gmail.com>
2022-08-23 23:48:19 +0200

    Add read/write methods to pref manager

--- app/src/main/java/hr/asimr/shows_asim/managers/SharedPreferencesManager.kt
+++ app/src/main/java/hr/asimr/shows_asim/managers/SharedPreferencesManager.kt
@@ -2,14 +2,33 @@ package hr.asimr.shows_asim.managers
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import hr.asimr.shows_asim.fragments.LOGIN_PREFERENCES
 
 object SharedPreferencesManager {
     private lateinit var sharedPrefs: SharedPreferences
 
-    fun init(context: Context){
+    fun init(context: Context) {
         sharedPrefs = context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
     }
 
+    fun writeString(key: String, value: String) {
+        sharedPrefs.edit {
+            putString(key, value)
+        }
+    }
 
+    fun readString(key: String, value: String): String? {
+        return sharedPrefs.getString(key, value)
+    }
+
+    fun writeBoolean(key: String, value: Boolean) {
+        sharedPrefs.edit {
+            putBoolean(key, value)
+        }
+    }
+
+    fun readBoolean(key: String): Boolean {
+        return sharedPrefs.getBoolean(key, false)
+    }
 }
